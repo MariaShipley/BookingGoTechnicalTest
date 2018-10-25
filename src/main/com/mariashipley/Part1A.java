48f83250e5334a372c4ced5b76fff124a766a666
Maria Shipley <mariashipley@live.co.uk>
2018-10-25 13:30:59 +0100

    Call check for valid input from main

--- src/main/com/mariashipley/Part1A.java
+++ src/main/com/mariashipley/Part1A.java
@@ -1,9 +1,16 @@
 package main.com.mariashipley;
 
+import static main.com.mariashipley.InputValidationUtils.isInputValid;
+import static main.com.mariashipley.InputValidationUtils.printExceptionMessage;
+
 public class Part1A
 {
     public static void main(String[] args)
     {
-
+        if (!isInputValid(args))
+        {
+            printExceptionMessage();
+            System.exit(0);
+        }
     }
 }
