49c23861cf9e4e872f98dc3ca1f0d51d225cda60
Maria Shipley <mariashipley@live.co.uk>
2018-10-26 13:01:20 +0100

    Added methods to search engine class to enable a basic search that takes in coordinates and prints results

--- src/main/com/mariashipley/SearchEngine.java
+++ src/main/com/mariashipley/SearchEngine.java
@@ -1,10 +1,66 @@
 package main.com.mariashipley;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import java.util.Collections;
+import java.util.List;
+
 public class SearchEngine
 {
     private static final String DAVE_TAXI_API = "https://techtest.rideways.com/dave";
+    private static final int CONNECTION_TIMEOUT = 2000;
 
-    public static String buildQuery(Coordinate pickUpLocation, Coordinate dropOffLocation)
+    /**
+     * Searches for car options for the given locations and prints in descending price order.
+     * @param pickUpLocation Coordinates of the pick-up location
+     * @param dropOffLocation Coordinates of the drop-off location
+     */
+    static void search(Coordinate pickUpLocation, Coordinate dropOffLocation)
+    {
+        URL url = buildQuery(pickUpLocation, dropOffLocation);
+        if (url == null)
+        {
+            return;
+        }
+
+        String response = makeApiCall(url);
+        ApiResponse apiResponse = deserialize(response);
+        if (apiResponse == null)
+        {
+            return;
+        }
+
+        List<RideOption> rideOptions = apiResponse.rideOptions;
+        if (rideOptions.isEmpty())
+        {
+            System.out.println("Sorry. There are no options available that match your query.");
+        }
+
+        Collections.sort(rideOptions, Collections.reverseOrder());
+        for (RideOption option : rideOptions)
+        {
+            System.out.println(option);
+        }
+    }
+
+    /**
+     * Constructs a URL of the API query with the given coordinates.
+     * @param pickUpLocation Coordinates of the pick-up location
+     * @param dropOffLocation Coordinates of the drop-off location
+     * @return URL of the query to access the API
+     */
+    public static URL buildQuery(Coordinate pickUpLocation, Coordinate dropOffLocation)
     {
         StringBuilder query = new StringBuilder(DAVE_TAXI_API);
         query.append("?pickup=");
@@ -16,6 +72,90 @@ public class SearchEngine
         query.append(",");
         query.append(dropOffLocation.getLongitude());
 
-        return query.toString();
+        try
+        {
+            return new URL(query.toString());
+        }
+        catch (MalformedURLException e)
+        {
+            System.out.println("Could not build URL for API query.");
+            return null;
+        }
+    }
+
+    /**
+     * Makes the HTTP call to the API.
+     * @param url The url of the API query
+     * @return string of API Response
+     */
+    private static String makeApiCall(URL url)
+    {
+        try
+        {
+            HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+            connection.setConnectTimeout(CONNECTION_TIMEOUT);
+            int responseCode = connection.getResponseCode();
+
+            if (responseCode == 200)
+            {
+                return inputStreamToString(connection.getInputStream());
+            }
+            else
+            {
+                System.out.println("Error code: " + responseCode);
+                if (responseCode == 400)
+                {
+                    System.out.println("Bad Request: Make sure your request was entered in the correct format.");
+                }
+                else if (responseCode == 500)
+                {
+                    System.out.println("Internal Server error: Wait a few seconds and re-submit your request");
+                }
+            }
+        }
+        catch (SocketTimeoutException timeoutException)
+        {
+            System.out.println("Connection timed-out: Re-submit your request.");
+        }
+        catch (IOException e)
+        {
+            System.out.println("Encountered a problem: please try again.");
+        }
+
+        return null;
+    }
+
+    /**
+     * Converts given input stream into a string.
+     * @param inputStream The input stream to be converted
+     * @return string conversion of input stream
+     */
+    private static String inputStreamToString(InputStream inputStream) throws IOException
+    {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder stringBuilder = new StringBuilder();
+        String line;
+
+        while ((line = bufferedReader.readLine()) != null)
+        {
+            stringBuilder.append(line);
+            stringBuilder.append("\n");
+        }
+
+        bufferedReader.close();
+
+        return stringBuilder.toString();
+    }
+
+    /**
+     * Deserializes the JSON string into and ApiResponse object
+     * @param jsonString The JSON string to be converted
+     * @return ApiResponse object
+     */
+    private static ApiResponse deserialize(String jsonString)
+    {
+        final GsonBuilder builder = new GsonBuilder();
+        final Gson gson = builder.create();
+        return gson.fromJson(jsonString, ApiResponse.class);
     }
 }
--- src/test/com/mariashipley/SearchEngineTest.java
+++ src/test/com/mariashipley/SearchEngineTest.java
@@ -3,17 +3,22 @@ package test.com.mariashipley;
 import main.com.mariashipley.Coordinate;
 import org.junit.jupiter.api.Test;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import static main.com.mariashipley.SearchEngine.buildQuery;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SearchEngineTest
+class SearchEngineTest
 {
     @Test
-    void buildQuery_Coordinates_ReturnsCorrectQuery()
+    void buildQuery_Coordinates_ReturnsCorrectQuery() throws MalformedURLException
     {
-        String expectedQuery = "https://techtest.rideways.com/dave?pickup=1.234,5.678&dropoff=-12.5,13.57";
+        URL expectedQuery = new URL("https://techtest.rideways.com/dave?pickup=1.234,5.678&dropoff=-12.5,13.57");
+
         Coordinate pickupLocation = new Coordinate(1.234, 5.678);
         Coordinate dropoffLocation = new Coordinate(-12.5, 13.57);
+
         assertEquals(buildQuery(pickupLocation, dropoffLocation), expectedQuery);
     }
 }
